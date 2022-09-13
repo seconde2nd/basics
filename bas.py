fd622bb8a8f030c4667bca4c57ea193c583d3e0c
primary Mdanas <59245356+Mdanass@users.noreply.github.com>
2022-09-13 17:34:04 +0530

    Update bas.py

--- bas.py
+++ bas.py
@@ -1,2 +1,3 @@
 name = input('your name pls')#getting an input from user
 print(f'hi{name}, how are you')#display input with hi
+# I would like to import a text to voice converstion library are you okay with this
